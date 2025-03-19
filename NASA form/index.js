3426cbdd34bf3cd2a989a00717917ea0421bbceb
StevenMsafiri <ianleonardmk47@gmail.com>
2025-03-19 23:55:58 +0300

    astrounaut form

---
+++ NASA form/index.js
@@ -0,0 +1,7 @@
+const loginForm = document.getElementById('login-form')
+
+loginForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const loginFormData = new FormData(loginForm)
+
+})
